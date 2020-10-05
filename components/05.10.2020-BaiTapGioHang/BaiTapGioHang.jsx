60af9f0d847d67f64179b6fb35ba3c20614f52f5
Kato030694 <longan.dbas@gmail.com>
2020-10-06 00:41:14 +0700

    Bo sung bai tap gio hang

--- components/05.10.2020-BaiTapGioHang/BaiTapGioHang.jsx
+++ components/05.10.2020-BaiTapGioHang/BaiTapGioHang.jsx
@@ -3,18 +3,35 @@ import DanhSachSanPhamGioHang from "./DanhSachSanPhamGioHang";
 import ModalGioHang from "./ModalGioHang";
 import data from "./data.json";
 export default class BaiTapGioHang extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      gioHang: [{"maSP": 1,
+    "tenSP": "VinSmart Live",
+    "giaBan": 5700000,
+    "hinhAnh": "./img/vsphone.jpg", 
+    "soLuong": 5}]
+    }
+  }
   render() {
+    let tongSoLuong = this.state.gioHang.reduce((tsl, sanPham, index)=>{
+      return tsl += sanPham.soLuong;
+    },0)
+    // Muốn tính tổng số lượng phải duyệt qua mảng giỏ hàng, số 0 là số đầu tiên của mảng duyệt
+    //Số đầu là 0, thì nó sẽ lấy 0 + cho số lượng đầu tiên, lần đầu là 5, sẽ trả là 5
+    // Lần thứ 2 thì sẽ lấy số lượng là 5 + với số lượng sản phẩm thứ 2.
+    // Cho đến khi duyệt hết mảng và trả về tongSoLuong
     return (
       <div class="container">
         <h3 className="text-center text-success">Bài Tập Giỏ Hàng</h3>
-        <ModalGioHang />
+        <ModalGioHang gioHang={this.state.gioHang}/>
         <div className="text-right">
           <span
             className="text-danger"
             style={{ cursor: "pointer", fontSize: "17px", fontWeight: "bold" }}
             data-toggle="modal" data-target="#modelId"
           >
-            Giỏ Hàng (0)
+            Giỏ Hàng ({tongSoLuong})
           </span>
         </div>
         <DanhSachSanPhamGioHang mangSanPham={data} />
--- components/05.10.2020-BaiTapGioHang/ModalGioHang.jsx
+++ components/05.10.2020-BaiTapGioHang/ModalGioHang.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 export default class ModalGioHang extends Component {
   render() {
+    const { gioHang } = this.props; // lấy giữ liệu giohang từ bài tập giỏ hàng
     return (
       <div
         className="modal fade"
@@ -12,9 +13,12 @@ export default class ModalGioHang extends Component {
         aria-hidden="true"
       >
         <div className="modal-dialog" role="document">
-          <div className="modal-content" style={{maxWidth:"800px", width:"800px"}}>
+          <div
+            className="modal-content"
+            style={{ maxWidth: "800px", width: "800px" }}
+          >
             <div className="modal-header">
-              <h5 className="modal-title">Modal title</h5>
+              <h5 className="modal-title">Product</h5>
               <button
                 type="button"
                 className="close"
@@ -24,7 +28,36 @@ export default class ModalGioHang extends Component {
                 <span aria-hidden="true">×</span>
               </button>
             </div>
-            <div className="modal-body">Body</div>
+            <div className="modal-body">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <td>Ma SP</td>
+                    <td>Hinh Anh</td>
+                    <td>Ten SP</td>
+                    <td>So Luong</td>
+                    <td>Don Gia</td>
+                    <td>Thanh Tien</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  {gioHang.map((sanPham, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{sanPham.maSP}</td>
+                        <td>
+                          <img src={sanPham.hinhAnh} width={50} height={50} />
+                        </td>
+                        <td>{sanPham.tenSP}</td>
+                        <td><button>A</button>{sanPham.soLuong}<button>B</button></td>
+                        <td>{sanPham.giaBan}</td>
+                        <td>{sanPham.soLuong * sanPham.giaBan}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
             <div className="modal-footer">
               <button
                 type="button"
